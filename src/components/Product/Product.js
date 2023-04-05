38e9aa9eb3ec0de10b4b42c21e274f0a3ee3c680
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-04-05 21:40:32 +0200

    Finalize ProductOptions

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,42 +1,17 @@
 import { useState } from 'react';
 import styles from './Product.module.scss';
-// import clsx from 'clsx';
-import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import ProductImage from '../ProductImage/ProductImage';
-import OptionColor from '../OptionColor/OptionColor';
-import OptionSize from '../OptionSize/OptionSize';
+import ProductOptions from '../ProductOptions/ProductOptions';
 
 const Product = ({id, name, title, basePrice, colors, sizes}) => {
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
-  const sizeHandle = e => {
-    e.preventDefault();
-    setCurrentSize(e.target.innerHTML);
-  }
-  
-  const colorHandle = e => {
-    e.preventDefault();
-    setCurrentColor(e.target.value);
-  }
-
   const getPrice = () => {
     let item = sizes.find( elem => elem.name === currentSize);
     return basePrice + item.additionalPrice;
-  }
-
-  const addToCart = e => {
-    e.preventDefault();
-    const product = {
-      name: {title},
-      price: getPrice(),
-      size: currentSize,
-      color: currentColor
-    }
-    console.log(product);
-    return product;
   }
 
   return (
@@ -47,13 +22,9 @@ const Product = ({id, name, title, basePrice, colors, sizes}) => {
           <h2 className={styles.name}>{title}</h2>
           <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
-          <OptionSize sizes={sizes} currentSize={currentSize} action={sizeHandle}/>
-          <OptionColor colors={colors} currentColor={currentColor} action={colorHandle} />
-          <Button className={styles.button} action={addToCart}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
+        <ProductOptions id={id} name={name} title={title} basePrice={basePrice} colors={colors} sizes={sizes} 
+          currentColor={currentColor} setCurrentColor={setCurrentColor}
+          currentSize={currentSize} setCurrentSize={setCurrentSize} getPrice={getPrice()} />
       </div>
     </article>
   )
--- src/components/ProductOptions/ProductOptions.js
+++ src/components/ProductOptions/ProductOptions.js
@@ -1,14 +1,54 @@
-//import PropTypes from 'prop-types';
-// import styles from './ProductImage.module.scss';
+import PropTypes from 'prop-types';
+import styles from './ProductOptions.module.scss';
+import Button from '../Button/Button';
+import OptionColor from '../OptionColor/OptionColor';
+import OptionSize from '../OptionSize/OptionSize';
 
-// const ProductOptions = props => {
-//     return (
+const ProductOptions = props => {
 
-//     )
-// }
+    const sizeHandle = e => {
+        e.preventDefault();
+        props.setCurrentSize(e.target.innerHTML);
+      }
+      
+      const colorHandle = e => {
+        e.preventDefault();
+        props.setCurrentColor(e.target.value);
+      }
+    
+      const addToCart = e => {
+        e.preventDefault();
+        console.log('Summary');
+        console.log('-------');
+        console.log(`Name: ${props.title}`);
+        console.log(`Price: ${props.getPrice}`);
+        console.log(`Size: ${props.currentSize}`);
+        console.log(`Color: ${props.currentColor}`);
+      }
 
-// ProductOptions.propTypes = {
+    return (
+        <form>
+          <OptionSize sizes={props.sizes} currentSize={props.currentSize} action={sizeHandle}/>
+          <OptionColor colors={props.colors} currentColor={props.currentColor} action={colorHandle} />
+          <Button className={styles.button} action={addToCart}>
+            <span className="fa fa-shopping-cart" />
+          </Button>
+        </form>
+    )
+}
 
-// }
+ProductOptions.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    basePrice: PropTypes.number.isRequired,
+    colors: PropTypes.array.isRequired,
+    sizes: PropTypes.array.isRequired,
+    currentColor: PropTypes.string.isRequired,
+    setCurrentColor: PropTypes.func.isRequired,
+    currentSize: PropTypes.string.isRequired,
+    setCurrentSize: PropTypes.func.isRequired,
+    getPrice: PropTypes.number.isRequired
+}
 
-// export default ProductOptions;
+export default ProductOptions;
